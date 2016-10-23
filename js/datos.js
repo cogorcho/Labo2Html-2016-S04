4a6bac76ebc6cabf5da545a79648899fa3981de8
Juan Arce <cogorcho@gmail.com>
2016-10-23 19:13:26 -0300

    Cambios basicos y algo de documentacion

--- js/datos.js
+++ js/datos.js
@@ -3,7 +3,8 @@
 //-----------------------------------------------------------
 
 var cuenta = 0;
-
+var preguntasExamen = 15;
+var examen = [];
 
 //--------------------------------------------------------------------
 //Evento click al menu, para agarrar los clicks sobre los links
@@ -25,8 +26,28 @@ var datos = [
     { clave: 'CODQ', valor: 'codigoq' }
 ];
 
+/*------------------------------------------
+    Esto reemplaza arrays locales q use.
+    Preguntas y respuestas no serian
+    necesarias. A revisar
+--------------------------------------------*/
+var listas = {
+    CATE : { data: [], length: 0 },
+    AREA : { data: [], length: 0 },
+    PREG : { data: [], length: 0 },
+    RESP : { data: [], length: 0 },
+    CODQ : { data: [], length: 0 }
+};
+
 var categoriaSeleccionada = undefined;
 var areaSeleccionada = undefined;
+var dataIsLocal = false; 
+/*----------------------------------------
+    Una vez q se hace el ajas y se trae
+    la data al localStorage, se setea a
+    true. Aunque aun no la estoy usando.
+------------------------------------------*/
+var dataIsLocal = false; 
 
 //--------------------------------------------------------------------
 // Cargar local storage con toda la data
@@ -76,13 +97,24 @@ function ajax(target) {
     //Enviamos la solicitud
     ajax_request.send();
 }
+//-------------------------------------------------------------------
+// Ya deberia estar todo en el cliente cargado.
+// Parseamos las keys para tener la data y poder usarla.
+//-------------------------------------------------------------------
+function controlCarga() {
+    cuenta = cuenta + 1; 
+    if (cuenta == datos.length)
+    {
+        cargarData();
+    }
+}
 
 //--------------------------------------------------------------------
 // Guarda en localStorage. 1ro arma la key en funcion de q data
 // esta procesando
 //--------------------------------------------------------------------
 function localstore(target, data) {
-
+    console.log('Vamo al server');
     var cuantos = data.length;
     switch (target.valor) {
         case 'categorias' : {
@@ -143,60 +175,56 @@ function localstore(target, data) {
             break;
         }
     }
+    dataIsLocal = true;
 }
 
-//-------------------------------------------------------------------
-// Traemos todo del server
-//-------------------------------------------------------------------
-cargarLocalStorage();
-
-
-//-------------------------------------------------------------------
-// Ya deberia estar todo en el cliente cargado.
-// Parseamos las keys para tener la data y poder usarla.
-//-------------------------------------------------------------------
-function controlCarga() {
-    cuenta = cuenta + 1; 
-    if (cuenta == datos.length)
-    {
-        cargarData();
-    }
+/*-------------------------------------------------------------------
+    Llamada a la funcion q Trae todo del server
+--------------------------------------------------------------------*/
+if (!dataIsLocal) {
+    cargarLocalStorage();    
 }
 
+/*-------------------------------------------------------------------
+    Traemos todo del server
+--------------------------------------------------------------------*/
 function cargarData() {
     var keys = Object.keys(localStorage);
     var cuantasKeys = keys.length;
-    var categorias = [];
-    var areas = [];
-    var preguntas = [];
-    var respuestas = [];
-    var codigoq = [];
-
 
     for (var i = 0; i < cuantasKeys; i++) {
         if (keys[i].search("CATE") === 0) {
-            categorias.push(JSON.parse(localStorage.getItem(keys[i])));
+            listas.CATE.data.push(JSON.parse(localStorage.getItem(keys[i])));
         }    
         if (keys[i].search("AREA") === 0) {
-            areas.push(JSON.parse(localStorage.getItem(keys[i])));
+            listas.AREA.data.push(JSON.parse(localStorage.getItem(keys[i])));
         }    
         if (keys[i].search("PREG") === 0) {
-            preguntas.push(JSON.parse(localStorage.getItem(keys[i])));
+            listas.PREG.data.push(JSON.parse(localStorage.getItem(keys[i])));
         }    
         if (keys[i].search("RESP") === 0) {
-            respuestas.push(JSON.parse(localStorage.getItem(keys[i])));
+            listas.RESP.data.push(JSON.parse(localStorage.getItem(keys[i])));
         }  
         if (keys[i].search("CODQ") === 0) {
-            codigoq.push(JSON.parse(localStorage.getItem(keys[i])));
+            listas.CODQ.data.push(JSON.parse(localStorage.getItem(keys[i])));
         }    
     }
 
-    armarRB({id: 'categoria', legend: 'Categorias', data: categorias});
-    armarRB({id: 'area', legend: 'Areas', data: areas});
+    listas.CATE.length = listas.CATE.data.length;
+    listas.AREA.length = listas.AREA.data.length;
+    listas.PREG.length = listas.PREG.data.length;
+    listas.RESP.length = listas.RESP.data.length;
+    listas.CODQ.length = listas.CODQ.data.length;
+
+    armarRB({id: 'categoria', legend: 'Categorias', data: listas.CATE.data});
+    armarRB({id: 'area', legend: 'Areas', data: listas.AREA.data});
     armarBoton();
 }
 
-
+/*-----------------------------------------------
+    Arma los radio button para seleccion
+    Area y Categoria
+-------------------------------------------------*/
 function armarRB(objData) {
     var cuantas = objData.data.length;
     var nav = document.getElementById('menu');
@@ -224,6 +252,9 @@ function armarRB(objData) {
     fset.addEventListener('click', clickRB);
 }
 
+/*-----------------------------------------------
+    Cada click en los RB, se guarda
+-------------------------------------------------*/
 function clickRB(ev) {
     var src = ev.srcElement;
     var rb = {
@@ -238,6 +269,9 @@ function clickRB(ev) {
         areaSeleccionada = rb;
 }
 
+/*--------------------------------------
+    Arma el boton para seleccionar data
+----------------------------------------*/
 function armarBoton() {
     var nav = document.getElementById('menu');
     var btn = document.createElement('input');
@@ -250,6 +284,10 @@ function armarBoton() {
     btn.addEventListener('click', armarTabla);   
 }
 
+/*----------------------------------------------
+    Con la seleccion (area y categoria) hecha
+    armamos la tabla con las preguntas
+-----------------------------------------------*/
 function armarTabla(ev) {
     if (categoriaSeleccionada === undefined) {
         alert('Seleccione una categoria');
@@ -260,6 +298,12 @@ function armarTabla(ev) {
         return;
     }
 
+    /*
+        Esto es mejorable. Si area y categoria no
+        cambiaron, se podria dejar todo como esta.
+        Pero, si algo cambio, la tabla de vuelve
+        a crear.
+    */
     var yaExiste = document.getElementById('tabla');
     if (yaExiste!=null) {
         yaExiste.parentNode.removeChild(yaExiste);
@@ -296,7 +340,13 @@ function armarTabla(ev) {
 
     tabla.addEventListener('click', rowSelected);
 }
-
+/*---------------------------------------------------------
+    Filtrar las preguntas para la categoria y area
+    seleccionadas. La data se busca en el localStorage.
+    El algoritmo es medio basico, pero se la banca.
+    Da para q se estudie y se entienda, pero solo busca
+    un juego de caracteres en un string (q es la Key).
+-----------------------------------------------------------*/
 function filterPreguntas(que, categoria, area) {
     var keys = Object.keys(localStorage);
     var cuantas = keys.length;
@@ -311,6 +361,11 @@ function filterPreguntas(que, categoria, area) {
     return lista;
 }
 
+/*-------------------------------------------------
+    Funcion para ordenar las preguntas por numero.
+    Como todo se guarda como strings, el sort
+    lo hace mal (1,10,100,101,11,110,111,12...)
+---------------------------------------------------*/
 function compare(a,b) {
     if (parseInt(a.preguntaid) < parseInt(b.preguntaid))
         return 1;
@@ -319,6 +374,12 @@ function compare(a,b) {
     return 0;
 }
 
+/*-------------------------------------------------------------
+    Filtrar las respuestas para la categoria, area y pregunta
+    seleccionadas. La data se busca en el localStorage.
+    Es lo mismo q con las preguntas, pero en este caso,
+    no hace falta el sort ya q las respuestas son ABCD
+--------------------------------------------------------------*/
 function filterRespuesta(que, categoria, area, preguntaid) {
     var keys = Object.keys(localStorage);
     var cuantas = keys.length;
@@ -332,7 +393,9 @@ function filterRespuesta(que, categoria, area, preguntaid) {
     return lista;
 }
 
-
+/*-------------------------------------------------------------
+    Captura el click sobre las filas de la tabla
+---------------------------------------------------------------*/
 function rowSelected(ev) {
     ev.preventDefault();
     var fila = ev.srcElement.parentElement;
@@ -359,16 +422,11 @@ function rowSelected(ev) {
     popup(data);
 }
 
-function strRespuestas(resps) {
-    var cuantas = resps.length;
-    var cr = "";
-    for (var i = 0; i < cuantas; i++) {
-        cr += resps[i].respuestaid + ': ' + resps[i].texto + "\n";
-    }
-    
-    return cr;
-}
-
+/*------------------------------------------------------------
+    Cuando se hace click sobre una pregunta, se muestra
+    un popup con las respuestas y capturar entrada del
+    usuario.
+---------------------------------------------------------------*/
 function popup(data) {
     var div0 = document.createElement('div');
     div0.id = 'text';
@@ -407,11 +465,28 @@ function popup(data) {
         responder.type = 'button';
         responder.value = 'Responder';
         responder.id = 'btnResponder';
-        var attCorrecta = document.createAttribute("data-correcta");
+
+        var attCorrecta = document.createAttribute('data-correcta');
         attCorrecta.value = data.correcta;
         responder.setAttributeNode(attCorrecta);
+        
         responder.addEventListener('click', procesarRespuesta);
         div1.appendChild(responder);
+
+
+        var cbxMarcar = document.createElement('input');
+        cbxMarcar.type = 'checkbox';
+        cbxMarcar.id = 'cbExamen';
+        cbxMarcar.setAttribute('data-preguntaid', data.nro);
+        cbxMarcar.setAttribute('data-categoriaid', categoriaSeleccionada.value);
+        cbxMarcar.setAttribute('data-areaid', areaSeleccionada.value);
+        var cbLabel = document.createElement("label");
+        cbLabel.id = 'cbLabel';
+        cbLabel.setAttribute("for",'cbExamen');
+        cbLabel.innerHTML = 'Agregar al examen';
+
+        div1.appendChild(cbxMarcar);
+        div1.appendChild(cbLabel);
 
         var closer = document.createElement('input');
         closer.type = 'button';
@@ -426,12 +501,29 @@ function popup(data) {
         div0.style.outline = "none";
 }
 
+/*----------------------------------------------
+    Cerrar la popup.
+------------------------------------------------*/
 function closePopup(ev) {
     ev.preventDefault();
     var el = document.getElementById('text');
+    var cbx = document.getElementById('cbExamen');
+    if (cbx.checked) {
+        var pregExamen = {
+            categoriaid: cbx.dataset.categoriaid,
+            areaid : cbx.dataset.areaid,
+            preguntaid : cbx.dataset.preguntaid
+        };
+        agregarAlExamen(pregExamen);
+    }
     document.body.removeChild(el);
 }
 
+/*-----------------------------------------------
+    Si el usuario selecciona una respuesta
+    y hace click en el boton de responder,
+    se ejecuta esta funcion.
+--------------------------------------------------*/
 function procesarRespuesta(ev) {
     ev.preventDefault();
     var rbs = document.getElementsByName('rbRespuestas');
@@ -447,4 +539,30 @@ function procesarRespuesta(ev) {
         alert('Correcto!');
     else
         alert('Intentelo nuevamente');
+}
+
+/*-----------------------------------------------
+    Esto esta verde mal! 
+    Me falta definir un monton de cosas
+-------------------------------------------------*/
+function agregarAlExamen(data) {
+    var key = 'EXAM_' + categoriaSeleccionada + '_' + areaSeleccionada + '_' +
+        data.preguntaid;
+
+    if (localStorage.getItem(key) === null) {
+        localStorage.removeItem(key);
+    }
+    localStorage.setItem(key, JSON.stringify(data));
+
+    var keys = Object.keys(localStorage);
+    var cuantas = keys.length;
+    var preguntasExamen = [];
+
+    for (var i = 0; i < cuantas; i++) {
+        if (keys[i].search("EXAM") === 0) {
+            preguntasExamen.push(JSON.parse(localStorage.getItem(keys[i])));
+        } 
+    }
+
+    console.dir(preguntasExamen);
 }
